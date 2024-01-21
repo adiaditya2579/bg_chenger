999fded1d560fdae845563ad7f2d06c368425d9c
Aditya Gupta <adiaditya2579@gmail.com>
2024-01-21 23:29:40 +0530

    add to main

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
         <button onClick={()=> setColor('#3a5a40')} className=" rounded-full py-2 px-4 text-white mx-2 my-2" style={{backgroundColor: "#3a5a40"}}>Green</button>
         <button onClick={()=> setColor("black")} className=" rounded-full text-white px-4 py-2 mx-2 my-2" style={{backgroundColor:"black"}}>Black</button>
         <button onClick={()=> setColor(" #f59e0b")} className=" rounded-full text-white px-4 py-2 mx-2 my-2  " style={{backgroundColor:"#f59e0b "}}>Yellow</button>
+        <button onClick={()=> setColor(" Blue")} className=" rounded-full text-white px-4 py-2 mx-2 my-2  " style={{backgroundColor:"Blue "}}>Blue</button>
         
       </div>
     </div>
